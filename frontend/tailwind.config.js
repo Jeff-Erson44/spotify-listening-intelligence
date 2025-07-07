4a1eb0bca3ef7a390ac1e32c812075e5bd68cea4
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2025-07-07 23:33:19 +0200

    [ADD] typo SF Pro

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"SFPro"', 'sans-serif'],
+      },
+      fontSize: {
+        header1: ['96px', { lineHeight: '96px', fontWeight: '500' }],
+        header2: ['40px', { lineHeight: '48px', fontWeight: '400' }],
+        body: ['16px', { lineHeight: '24px', fontWeight: '400' }],
+        caption: ['12px', { lineHeight: '16px', fontWeight: '300' }],
+      },
+    },
   },
   plugins: [],
 }
