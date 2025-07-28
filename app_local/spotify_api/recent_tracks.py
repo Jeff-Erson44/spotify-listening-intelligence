e0d5240f60a7cba444030ac68a870d25ef074310
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2025-07-28 22:26:04 +0200

    Delete app_local directory

--- app_local/spotify_api/recent_tracks.py
+++
@@ -1,4 +0,0 @@
-def get_recent_tracks(sp, limit=50):
-    #Récupère les derniers morceaux écoutés par l'utilisateur
-    results = sp.current_user_recently_played(limit=50)
-    return results.get('items', [])
